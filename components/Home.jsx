82f4266aa0809da137cf1172aeeb5fab81995e23
bluVshal <blu.vishal@gmail.com>
2025-07-08 00:02:41 +0400

    Minor changes in Home Component

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { StyleSheet, View } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { Layout, Text } from '@ui-kitten/components';
 import ListOfBooks from './ListOfBooks';
@@ -10,9 +11,17 @@ const Home = () => {
     return (
         <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text category='h1'>{t('app.title')}</Text>
-            <ListOfBooks />
+            <View style={styles.listContainer}>
+                <ListOfBooks />
+            </View>
         </Layout>
     )
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+    listContainer : {
+       margin : 10
+    }
+});

496afd10797b16cc285ee4737877cd91dee85a3e
bluVshal <blu.vishal@gmail.com>
2025-07-07 22:36:52 +0400

    Modifications added to app

--- components/ListOfBooks.jsx
+++ components/ListOfBooks.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { Button, Input, Layout } from '@ui-kitten/components';
 
 const ListOfBooks = () => {
-  return (
-    <View>
-        <Text>AAA</Text>
-    </View>
-  )
+    const { t, i18n } = useTranslation();
+    const [bookTitle, setBookTitle] = useState('');
+
+    return (
+        <Layout
+            style={styles.container}
+            level='1'
+        >
+            <Input placeholder='Enter book title'
+                value={bookTitle}
+                onChangeText={nextValue => setBookTitle(nextValue)}
+            />
+            <Button
+                style={styles.button}
+                status='info'
+            >
+                {t('app.buttons.search').toUpperCase()}
+            </Button>
+        </Layout>
+    )
 }
 
-export default ListOfBooks
+export default ListOfBooks;
+
+;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    button: {
+        margin: 2,
+    },
+    controlContainer: {
+        borderRadius: 4,
+        margin: 2,
+        padding: 6,
+        justifyContent: 'center',
+        backgroundColor: '#3366FF',
+    },
+});
